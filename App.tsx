55ec762571037400d5770f9e685a0400da459aba
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-01 22:29:02 +0700

    set: pod;

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={{backgroundColor: 'white'}}>APP</View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
