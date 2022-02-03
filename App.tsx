3917ce6f0a2eccd64b36c17edf956de481ef3be4
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-03 22:37:55 +0700

    set: bottomTab

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./src/navigation/Tabs";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar barStyle="default" />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
 
